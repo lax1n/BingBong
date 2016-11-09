49b6fc3e2f820a0fe76d733cacf900060ff78fec
avalds <avalds@gmail.com>
2016-11-09 21:48:08 +0100

    Give me data now returns data in a more user friendly format

--- src/utils/find_duplicates.js
+++ src/utils/find_duplicates.js
@@ -3,7 +3,7 @@ import {getAllTEIsByOrganizationAndProgram} from '../actions/tei_actions';
 
 export function giveMeData(){
 	return getAllTEIsByOrganizationAndProgram("DiszpKrYNg8", "ur1Edk5Oe2n").then((everything) => {
-		var i;
+		var i, j;
 		//var headers = [];
 		var instances = [];
 		console.log("everything: " + everything);
@@ -14,18 +14,19 @@ export function giveMeData(){
 		var headerCount = everything.headers.length;
 		var instanceCount = everything.rows.length;
 		for (i = 0; i < instanceCount; i++) {
-			for (i = 0; i < headerCount; i++) {
-				eval("instances.push({"+everything.headers[i].column+":"+ everything.rows[i]+"})");
+			for (j = 0; j < headerCount; j++) {
+				//console.log('instances.push({"'+everything.headers[j].column+'":"'+ everything.rows[i][j]+'"})');
+				eval('instances.push({"'+everything.headers[j].column+'":"'+ everything.rows[i][j]+'"})');
 			}
 		}
 		return instances;
 	}).catch((e) => {
 		console.log('Error while loading everything', e.message);
+		return -1;
 	});
 }
 export function findPeopleWithTheSameName(){
-	/*giveMeData().then((instances) => {
-		console.log(instances);
+	giveMeData().then((instances) => {
 		for (var i = 0; i < instances.length; i++) {
 			/*instances.each(key, value){
 				console.log("Hello");
@@ -33,12 +34,13 @@ export function findPeopleWithTheSameName(){
 			/*instances.each((instance, i) => {
 				console.log(instance);
 				console.log(i);
-			});
-			console.log(instances[i].City)
+			});*/
+			//console.log("Here:");
+			//console.log(instances[i].City)
 			for (var key in instances[i]) {
-				console.log("key: "+key);
+				//console.log("key: "+key+" values: "+instances[i][key])
 			}
 		}
-	});*/
+	});
 	return "WARNING! joke ahead--------------------------------------------------------------> (There is no joke)"
 }
