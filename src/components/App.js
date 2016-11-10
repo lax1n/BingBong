eaf8ccd6ab9f22a29f14a69cfdefb8303b80f126
avalds <avalds@gmail.com>
2016-11-10 16:10:01 +0100

    Another merge conflict should be resolved

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import logo from '../images/logo_new.png';
 import '../styles/App.css';
 import '../libs/bootstrap.min.css';
 
-import {testFunction, findDuplicatePeople} from '../utils/find_duplicates.js';
+import {getTip, findDuplicatePeople} from '../utils/find_duplicates.js';
 
 import TabBar from './shared/tab_bar.js';
 //import SelectPage from './pages/select';
@@ -11,19 +11,18 @@ import TabBar from './shared/tab_bar.js';
 
 class App extends Component {
 	render() {
+		console.log("Render called in App.js")
 		return (
-			<div className='container-fluid'>
-				<div className='row'>
-					<div className="App">
-						<div className="App-header">
-							<img src={logo} className="App-logo" alt="logo" />
-						</div>
-					</div>
-				</div>
-				<div className='row'>
-					<TabBar />
-				</div>
-			</div>
+  		    <div className='container-fluid'>
+  		      <div className='row'>
+  		        <div className="App">
+  		          <div className="App-header">
+  		            <img src={logo} className="App-logo" alt="logo" />
+  		          </div>
+		            <TabBar />
+  	            </div>
+  		      </div>
+  		  </div>
 		);
 	}
 }
