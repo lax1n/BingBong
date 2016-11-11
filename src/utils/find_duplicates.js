9085ee99c4c0ddc5a6538410250e48d585433728
avalds <avalds@gmail.com>
2016-11-11 15:15:44 +0100

    Tidied up findDuplicatePeople

--- src/utils/find_duplicates.js
+++ src/utils/find_duplicates.js
@@ -25,7 +25,7 @@ export function parseQueryResults(response){
 	return teis;
 }
 // This function does not take empty values into account, e.g last name "" will be evaluated as a duplicate if both are empty.
-export function isDuplicate(obj1, obj2){
+export function isDuplicate(obj1, obj2, test_params){
 	/*
 	const attributes = {
 		"Instance": false,
@@ -34,25 +34,44 @@ export function isDuplicate(obj1, obj2){
 		"Date of birth": true,
 	}
 	*/
-	return (
+	//console.log(test_params);
+	for (var i = 0; i<test_params.length; i++){
+		if((obj1[test_params[i]] === obj2[test_params[i]]) === false){
+			return false;
+		}// && ((obj1[test_params[i]] === "") || (obj1[test_params[i]] === "")))
+	}
+	return true
+	/*return (
 		//(obj1["Instance"] !== obj2["Instance"]) &&
 		(obj1["First name"] === obj2["First name"]) &&
 		(obj1["Last name"] === obj2["Last name"]) &&
 		(obj1["Date of birth"] === obj2["Date of birth"])
-	)
+	)*/
+}
+
+export function contains(needle, indexes) {
+	for(var i = 0; i<indexes.length; i++){
+		if(needle == indexes[i]){
+			return true;
+		}
+	}
+	return false;
 }
 
 export function findDuplicatePeople(teis){
 	let duplicates = [];
+	var test_params = ["First name", "Last name", "Date of birth"];
+	let duplicate_indexes = []
 	teis.forEach((tei, i) => {
 		let tempDuplicates = []
 		teis.forEach((tempTei, j) => {
-			if(isDuplicate(tei, tempTei)){
+			if(i !== j && !contains(i, duplicate_indexes) && isDuplicate(tei, tempTei, test_params)){
 				// Add duplicate to tempDuplicates & remove it from teis to avoid redundancy later
-				tempDuplicates.push(tempTei);
+				tempDuplicates.push(teis[j]);
+				duplicate_indexes.push(j);
 
 				// NOTE TO SELF: THIS MIGHT DELETE THE WRONG ELEMENT FOR FUCK SAKEEEEE ARRRRGHHHHHHH
-				teis.splice(j, 1);
+				//teis.splice(j, 1);
 				//console.log(j);
 				//console.log(teis[j]);
 
