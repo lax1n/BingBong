a1d5d09cb5627a8276aa0ecc0af66163ad2db265
avalds <avalds@gmail.com>
2016-11-20 17:47:08 +0100

    Changed back to the way lax1n fetch url

--- src/actions/singleton_actions.js
+++ src/actions/singleton_actions.js
@@ -2,16 +2,16 @@ import {serverUrl, fetchOptionsGet, onlySuccessResponses} from './api';
 const pageSize = 100;
 
 export function getAllSingletonsByOrganization(orgUnitId){
-    //return fetch(`${serverUrl}events?orgUnit=${orgUnitId}&skipPaging=true`, fetchOptionsGet)
-    return fetch(`${serverUrl}events?orgUnit=${orgUnitId}&pageSize=${pageSize}`, fetchOptionsGet)
+    return fetch(`${serverUrl}events?orgUnit=${orgUnitId}&skipPaging=true`, fetchOptionsGet)
+    //return fetch(`${serverUrl}events?orgUnit=${orgUnitId}&pageSize=${pageSize}`, fetchOptionsGet)
         .then(onlySuccessResponses)
         .then(response => response.json())
         .then(( {events} ) => events);
 }
 
 export function getAllSingletonsByOrganizationAndProgram(orgUnitId, programId){
-    //return fetch(`${serverUrl}events?orgUnit=${orgUnitId}&program=${programId}&skipPaging=true`, fetchOptionsGet)
-    return fetch(`${serverUrl}events?orgUnit=${orgUnitId}&program=${programId}&pageSize=${pageSize}`, fetchOptionsGet)
+    return fetch(`${serverUrl}events?orgUnit=${orgUnitId}&program=${programId}&skipPaging=true`, fetchOptionsGet)
+    //return fetch(`${serverUrl}events?orgUnit=${orgUnitId}&program=${programId}&pageSize=${pageSize}`, fetchOptionsGet)
         .then(onlySuccessResponses)
         .then(response => response.json())
         .then(( {events} ) => events);
