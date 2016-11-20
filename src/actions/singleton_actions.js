de7db96265fbf376973b45bae94f53f734533634
lax1n <august.haug@gmail.com>
2016-11-20 17:34:19 +0100

    Use skipPaging instead of pageSize

--- src/actions/singleton_actions.js
+++ src/actions/singleton_actions.js
@@ -1,14 +1,14 @@
 import {serverUrl, fetchOptionsGet, onlySuccessResponses} from './api';
 
 export function getAllSingletonsByOrganization(orgUnitId){
-    return fetch(`${serverUrl}events?orgUnit=${orgUnitId}&pageSize=100000`, fetchOptionsGet)
+    return fetch(`${serverUrl}events?orgUnit=${orgUnitId}&skipPaging=true`, fetchOptionsGet)
         .then(onlySuccessResponses)
         .then(response => response.json())
         .then(( {events} ) => events);
 }
 
 export function getAllSingletonsByOrganizationAndProgram(orgUnitId, programId){
-    return fetch(`${serverUrl}events?orgUnit=${orgUnitId}&program=${programId}&pageSize=100000`, fetchOptionsGet)
+    return fetch(`${serverUrl}events?orgUnit=${orgUnitId}&program=${programId}&skipPaging=true`, fetchOptionsGet)
         .then(onlySuccessResponses)
         .then(response => response.json())
         .then(( {events} ) => events);
