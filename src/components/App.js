cf100b55a740cd08c58845f767d3e1ee858f8a37
lax1n <august.haug@gmail.com>
2016-11-20 16:29:03 +0100

    Cleanup (remove unused component & comment)

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,6 @@ import '../libs/bootstrap.min.css';
 
 import TabBar from './shared/tab_bar.js';
 
-//import {findTEIDuplicatesByOrganization} from "../utils/find_duplicates"
-
 
 class App extends Component {
 	render() {
