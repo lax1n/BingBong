67144d86a9c74de2efdc50a248004c561666bac6
avalds <avalds@gmail.com>
2016-11-21 11:46:33 +0100

    Ids are now properly converted to their display name

--- src/utils/singleton_dup_finder.js
+++ src/utils/singleton_dup_finder.js
@@ -4,19 +4,16 @@ import {parseSingletonQueryResults} from './singleton_parsers'
 
 export function findSingletonDuplicatesByOrganizationAndProgram(orgUnit, program){
 	return getAllSingletonsByOrganizationAndProgram(orgUnit, program).then((response) => {
-		let singletons,  categories
-		[singletons, categories] = parseSingletonQueryResults(response);
-		//XXX We need to transform the categories here so that the can be passed properly
-		const duplicates = findDuplicatePeople(singletons, ["oZg33kd9taw", "a3kGcGDCuk6", "UXz7xuGCEhU"],  ["Blood type"]);
+		let singletons = parseSingletonQueryResults(response);
+		const duplicates = findDuplicatePeople(singletons, undefined,  ["Blood type"]);
 		return duplicates;
 		//return response;
 	});
 }
 export function findSingletonDuplicatesByOrganization(orgUnit){
 	return getAllSingletonsByOrganization(orgUnit).then((response) => {
-		let singletons,  categories
-		[singletons, categories] = parseSingletonQueryResults(response);
-		const duplicates = findDuplicatePeople(singletons, ["oZg33kd9taw", "a3kGcGDCuk6", "UXz7xuGCEhU"],  ["Blood type"]);
+		let singletons = parseSingletonQueryResults(response);
+		const duplicates = findDuplicatePeople(singletons, undefined,  ["Blood type"]);
 		return duplicates;
 	});
 }
--- src/utils/singleton_parsers.js
+++ src/utils/singleton_parsers.js
@@ -1,43 +1,49 @@
 import {includes} from 'lodash';
+import {getDataElements} from "../actions/data_element_actions"
 export function parseSingletonQueryResults(response){
 	let i, j;
 	let singletons = [];
+	getDisplayNameConverter(response).then((displayNameConverter) => {
+		let attributesLength;
+		let responseCount = response.length;
+		console.log("Hello");
+		console.log(displayNameConverter);
+		for (i = 0; i < responseCount; i++) {
+			singletons[i] = {};
+			attributesLength = response[i].dataValues.length;
+			for (j = 0; j < attributesLength; j++) {
+				singletons[i][displayNameConverter[response[i].dataValues[j].dataElement]] = response[i].dataValues[j].value;
+			}
+			///XXX Do not remove, we might uncomment the addition of the other keys
+			/*for(let key in response[i]){
+				if (response[i].hasOwnProperty(key) && key !== "dataValues") {
+					singletons[i][key] = response[i][key];
+				}
+			}*/
+		}
+		console.log("Singletons")
+		console.log(singletons)
+		return singletons
+	});
+}
+function getDisplayNameConverter(response){
+	let i, j;
 	let categories = [];
 	let attributesLength;
 	let responseCount = response.length;
-	//console.log("Quaky");
-	//console.log(response);
-	//console.log(responseCount);
-	//console.log(response[0].dataValues);
-	//console.log(response[0].dataValues.length);
 	for (i = 0; i < responseCount; i++) {
-		singletons[i] = {};
-		//console.log("singletons")
-		//console.log(singletons)
 		attributesLength = response[i].dataValues.length;
-		//console.log(response[i].dataValues.length);
 		for (j = 0; j < attributesLength; j++) {
-			//console.log("j: " + j);
-			//console.log(response[i].dataValues[j].dataElement);
-			//console.log(response[i].dataValues[j].value);
-			singletons[i][response[i].dataValues[j].dataElement] = response[i].dataValues[j].value;
 			if(!(includes(categories, response[i].dataValues[j].dataElement))){
 				categories.push(response[i].dataValues[j].dataElement);
 			}
 		}
-		for(let key in response[i]){
-			if (response[i].hasOwnProperty(key) && key !== "dataValues") {
-				singletons[i][key] = response[i][key];
-			}
-		}
-		/*singletons[i]['event'] = response[i].event;
-		singletons[i]['program'] = response[i].program;*/
-
 	}
-	console.log("Singletons")
-
-	console.log(singletons)
-	console.log("categories")
-	console.log(categories)
-	return [singletons, categories];
+	return getDataElements(categories).then((displayNameObjects) => {
+		let my_converter = {};
+		for(i = 0; i<displayNameObjects.length; i++){
+			my_converter[displayNameObjects[i].id] = displayNameObjects[i].name
+		}
+		return my_converter;
+	});
 }
