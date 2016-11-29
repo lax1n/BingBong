047e88f28395d5ea89739e736b60fc7e97b511e9
mahastya <mahastya@ifi.uio.no>
2016-11-29 22:47:10 +0100

    new logos

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import {serverUrl, basicAuth, fetchOptionsGet, onlySuccessResponses,} from '../actions/api.js';
 
 import '../styles/App.css';
 import '../libs/bootstrap.min.css';
 
 import TabBar from './shared/tab_bar.js';
+
+/*componentDidMount(){
+  return fetch(`${serverUrl}dataStore/BingBong/${key}`, {method : 'GET'
+
+      
+    }
+    );
+}*/
 
 class App extends Component {
 	render() {
