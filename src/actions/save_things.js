1a0b405add85a31a45e8bde619ca8dea4a63322e
mahastya <mahastya@ifi.uio.no>
2016-11-29 13:07:42 +0100

    cant remember last time i pushed

--- src/actions/save_things.js
+++ src/actions/save_things.js
@@ -1,13 +1,16 @@
 import {serverUrl, basicAuth, fetchOptionsGet, onlySuccessResponses,} from './api';
 
 export function saveThings(key,things){
+    let items = getThings(key);
+    console.log("forgot to add this: "+items);
+    //items.push(things);
 	return fetch(`${serverUrl}dataStore/BingBong/${key}`, { method: 'PUT', 
 		headers: {
         'Authorization': basicAuth,
         'Content-Type': 'application/json',
     },
 		 body: JSON.stringify({
-            things,
+            items,
  	}),
 	})
         .then(onlySuccessResponses)
@@ -18,5 +21,7 @@ export function getThings(key){
     return fetch(`${serverUrl}dataStore/BingBong/${key}`, fetchOptionsGet)
         .then(onlySuccessResponses)
         .then(response => response.json())
-        .then(( {things} ) => things);
+        .then((things) => things);
 }
+
+//curl "https://play.dhis2.org/demo/api/dataStore/BingBong/[key]" -X POST -H "Content-Type: application/json" -d "{\"BingBong\":\"recents\"}" -u admin:district -v 
