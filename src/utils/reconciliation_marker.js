06f71488d50d7bd50ec2629c61e9747a27832357
avalds <avalds@gmail.com>
2016-11-29 20:42:04 +0100

    tidied up reconciliationMarker

--- src/utils/reconciliation_marker.js
+++ src/utils/reconciliation_marker.js
@@ -1,19 +1,12 @@
 import {saveThings, getThings} from "../actions/save_things.js"
 
-export function pushMarkedToServer(duplicates){//These duplicates have been confirmed by an admin.
-	console.log(duplicates);
-	//saveThings('markedForReconciliation', JSON.stringify(duplicates));
-	saveThings('markedForReconciliation', duplicates, 'DELETE');
-	saveThings('markedForReconciliation', duplicates, 'POST');
-}
-
-export function updateMarked(duplicates, type){
+export function updateMarked(duplicates, type){//These duplicates have been confirmed by an admin.
 	let key = type+"_duplicates";
 	return getThings(key).then(function(prev){
 		console.log("prev");
 		console.log(prev);
 		//saveThings(key, duplicates, 'DELETE');
 		//saveThings(key, duplicates, 'POST');
-		return saveThings(key, duplicates.concat(prev), 'PUT');
+		return saveThings(key, duplicates.concat(prev.things), 'PUT');
 	});
 }
