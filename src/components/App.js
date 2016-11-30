b3fcbb9a6c3ff48ba79fbcc49fdf5218ac3397d5
mahastya <mahastya@ifi.uio.no>
2016-11-30 16:02:24 +0100

    checking if data store exists, if no...

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,19 @@ import '../libs/bootstrap.min.css';
 
 import TabBar from './shared/tab_bar.js';
 
-/*componentDidMount(){
-  return fetch(`${serverUrl}dataStore/BingBong/${key}`, {method : 'GET'
-
-      
-    }
-    );
-}*/
+componentDidMount(){
+  try{ 
+    fetch(`${serverUrl}dataStore/BingBong/`, {method : 'GET',
+    headers: {
+        'Authorization': basicAuth,
+        'Content-Type': 'application/json',
+    },
+    }).then(onlySuccessResponses).then(response => response.json());
+  }
+  catch(error){
+    console.log("bingbong eksisterer ikke");
+  }
+}
 
 class App extends Component {
 	render() {
