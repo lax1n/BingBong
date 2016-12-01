3e68be4f28b43721a35606dbcc097c0dde5b261a
August <august.haug@gmail.com>
2016-12-01 16:45:34 +0100

    Remove unused packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './components/App';
 import './styles/index.css';
-
-injectTapEventPlugin();
 
 ReactDOM.render(
   <App />,
