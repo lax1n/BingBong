aaae6f2016f08a25092168ba5fa16f516455984e
mahastya <mahastya@ifi.uio.no>
2016-12-01 13:30:16 +0100

    updated about page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import bong from '../../images/bingbong.jpg';
 
 class About extends Component {
 
@@ -6,8 +7,12 @@ class About extends Component {
 		return(
 			<div className='text-center'>
 				<br />
-                <p>Use Tracked Entity Instances to gather duplicates of TEIs </p>
-                <p>Use Singletons to gather duplicates of single events that are not associated with persons</p>
+                <p><b>The deduplicator</b> is for gathering elements that are duplicates, with certain misspellings and such, and offering the user an option to mark them to be reconciled</p>
+                <p>Use <i>Tracked Entity Instances</i> to gather duplicates of TEIs </p>
+                <p>Use <i>Singletons</i> to gather duplicates of single events that are not associated with persons</p>
+                <br />
+                <p>Created by IFI group BingBong; August Haug Hem, Mahasty Assi, Åvald Åslaugson Sommervoll</p>
+                <img src={bong} className="Img-size" />
 			</div>
 		);
 	}
