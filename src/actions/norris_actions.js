90bbdc080858483cf60784ca572bb3dc266090eb
avalds <avalds@gmail.com>
2016-12-01 13:36:17 +0100

    removed norris action

--- src/actions/norris_actions.js
+++
@@ -1,9 +0,0 @@
-import {onlySuccessResponses} from './api';
-
-// Add '&paging=false' to the url to include ALL units at once. Might be time consuming for testing.
-export function getNorrisJoke(){
-    return fetch(`http://api.icndb.com/jokes/random`)
-        .then(onlySuccessResponses)
-        .then(response => response.json())
-        .then(({ value }) => value);
-}
